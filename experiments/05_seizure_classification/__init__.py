f85a457914b8cd1df9f371f87827a1d367002ec4
BlakeJC94 <blakejamescook@gmail.com>
2024-03-23 21:26:04 +1100

    Add TS model
    Former-commit-id: b971dcbd6b1663ba2986c58bbd833976cb0c1e0d
    Former-commit-id: 49c91eb90c99e6124a94a3990d2b3149cd952639

--- experiments/05_seizure_classification/__init__.py
+++ experiments/05_seizure_classification/__init__.py
@@ -144,6 +144,217 @@ class TanhClip(nn.Module):
         return torch.tanh(x / self.abs_bound) * self.abs_bound
 
 
+class BasicBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        features: int,
+        kernel_size: int,
+        **conv_kwargs,
+    ):
+        super().__init__()
+        self.conv1 = nn.Conv1d(in_channels, features, kernel_size, **conv_kwargs)
+        self.bn1 = nn.BatchNorm1d(num_features=features)
+        self.act = nn.LeakyReLU()
+        self.conv2 = nn.Conv1d(features, features, kernel_size, **conv_kwargs)
+        self.bn2 = nn.BatchNorm1d(num_features=features)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.act(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        length = out.shape[-1]
+        start = (x.shape[-1] - length) // 2
+        x = torch.narrow(x, dim=-1, start=start, length=length)
+        return self.act(out + x)
+
+
+class DownConvBlock(nn.Sequential):
+    def __init__(self, in_channels, features, pool):
+        super().__init__()
+        self.in_channels = in_channels
+        self.features = features
+        self.scale = pool
+
+        self.conv = nn.Conv1d(in_channels, features, kernel_size=pool, stride=pool)
+        self.bn = nn.BatchNorm1d(features)
+
+
+class EncoderBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        features: int,
+        kernel_size: int,
+        pool: int,
+        **conv_kwargs,
+    ):
+        super().__init__()
+        self.pool = pool
+
+        self.expand = DownConvBlock(in_channels, features, pool)
+        self.block = BasicBlock(features, features, kernel_size)
+
+    def forward(self, x) -> torch.Tensor:
+        x = self.expand(x)
+        x = self.block(x)
+        return x
+
+
+class UpConvBlock(nn.Sequential):
+    def __init__(self, in_channels, features, scale):
+        super().__init__()
+        self.in_channels = in_channels
+        self.features = features
+        self.scale = scale
+
+        self.conv = nn.Conv1d(in_channels, features, scale, padding="valid")
+        self.bn = nn.BatchNorm1d(num_features=features)
+        self.act = nn.LeakyReLU()
+
+    def forward(self, x):
+        x = nn.functional.interpolate(
+            x,
+            size=self.scale * (x.shape[-1] + 1) - 1,
+            mode="nearest-exact",
+        )
+        return super().forward(x)
+
+from math import floor, ceil, sqrt
+
+class DecoderBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        features: int,
+        scale: int,
+        kernel_size: int,
+        **conv_kwargs,
+    ):
+        super().__init__()
+        self.scale = scale
+
+        self.upconv = UpConvBlock(in_channels, features, scale)
+        self.contract = nn.Sequential(
+            nn.Conv1d(2 * features, features, kernel_size),
+            nn.BatchNorm1d(features),
+        )
+        self.block = BasicBlock(features, features, kernel_size, **conv_kwargs)
+
+    def forward(self, x, res) -> torch.Tensor:
+        x = self.upconv(x)
+        x = torch.cat(self.centre_crop(x, res), dim=1)
+        x = self.contract(x)
+        return self.block(x)
+
+    @staticmethod
+    def centre_crop(x, res) -> Tuple[torch.Tensor, torch.Tensor]:
+        x_len, res_len = x.shape[-1], res.shape[-1]
+        if x_len == res_len:
+            return x, res
+
+        l_crop = floor(int(abs(x_len - res_len)) / 2)
+        r_crop = ceil(int(abs(x_len - res_len)) / 2)
+
+        if x_len < res_len:
+            res = res[..., l_crop:(-r_crop)]
+        else:
+            x = x[..., l_crop:(-r_crop)]
+
+        return x, res
+
+
+class Backbone(nn.Module):
+    def __init__(
+        self,
+        n_channels: int,
+        n_classes: int,
+        dilation: int = 1,
+        kernel_size: int = 9,
+        n_features: int = 32,
+        padding: str = "valid",
+    ):
+        super().__init__()
+
+        self.n_channels = n_channels
+        self.n_classses = n_classes
+        self.kernel_size = kernel_size
+        self.n_features = n_features
+        self.padding = padding
+
+        pools = [2, 2, 2, 2]
+        expansions = [sqrt(2) for _ in pools]
+        filters = self._get_filters(n_features, expansions=expansions)
+
+        self.encoder = nn.ModuleList(
+            [
+                EncoderBlock(n_channels, filters[0], kernel_size, 1, padding=padding),
+                EncoderBlock(filters[0], filters[1], kernel_size, pools[0], padding=padding),
+                EncoderBlock(filters[1], filters[2], kernel_size, pools[1], padding=padding),
+                EncoderBlock(filters[2], filters[3], kernel_size, pools[2], padding=padding),
+            ]
+        )
+
+        self.bridge = EncoderBlock(filters[3], filters[4], kernel_size, pools[3], padding=padding)
+
+        self.decoder = nn.ModuleList(
+            [
+                DecoderBlock(filters[4], filters[3], pools[3], kernel_size, padding=padding),
+                DecoderBlock(filters[3], filters[2], pools[2], kernel_size, padding=padding),
+                DecoderBlock(filters[2], filters[1], pools[1], kernel_size, padding=padding),
+                DecoderBlock(filters[1], filters[0], pools[0], kernel_size, padding=padding),
+            ]
+        )
+
+        dense_blocks = [
+            nn.Conv1d(
+                in_channels=filters[0],
+                out_channels=n_classes,
+                kernel_size=1,
+            ),
+            nn.Tanh(),
+        ]
+        self.dense = nn.Sequential(*dense_blocks)
+
+        self.apply(self._init_conv_weight_bias)
+        self.apply(self._init_bn_weight)
+
+    def _init_conv_weight_bias(self, module):
+        if isinstance(module, (torch.nn.Conv1d)):
+            nn.init.kaiming_normal_(module.weight, mode="fan_out", nonlinearity="leaky_relu")
+            nn.init.constant_(module.bias, 0.01)
+            print("Updated", module)
+
+    def _init_bn_weight(self, module):
+        if isinstance(module, BasicBlock):
+            nn.init.constant_(module.bn2.bias, 0)
+
+    @staticmethod
+    def _get_filters(n_features, expansions):
+        fs = [n_features]
+        for expansion in expansions:
+            fs.append(fs[-1] * expansion)
+        return [int(f) for f in fs]
+
+    def forward(self, x):
+        _n_batches, _n_channels, n_timesteps = x.shape
+
+        residuals = []
+        for i, block in enumerate(self.encoder):
+            x = block(x)
+            residuals.append(x)
+
+        x = self.bridge(x)
+
+        for i, (block, residual) in enumerate(zip(self.decoder, residuals[::-1])):
+            x = block(x, residual)
+
+        x = self.dense(x)
+        return x
+
+
 ## Ensemble
 class MyModel(nn.Module):
     def __init__(self, n_channels, n_classes, spectrogram_transform, tanh_clip):
@@ -156,6 +367,11 @@ class MyModel(nn.Module):
         # Create network for time series
         self.model_time_series = nn.Sequential(
             TanhClip(tanh_clip),
+            BackBone(
+                n_channels=n_channels,
+                n_classes=n_classes,
+                padding="valid",
+            ),
             ...
         )
         ...
