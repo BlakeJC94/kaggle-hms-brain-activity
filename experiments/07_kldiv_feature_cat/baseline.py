09cea81d2cb04b74431ff6a36bbc556b2301917b
BlakeJC94 <blakejamescook@gmail.com>
2024-03-24 21:16:00 +1100

    select the weights!
    Former-commit-id: 7d4cf324ecd49946dc278256040e16a02cf033e9
    Former-commit-id: cb2aff1a2a576493d9ade017a7c45f5e6c0707b5

--- experiments/07_kldiv_feature_cat/baseline.py
+++ experiments/07_kldiv_feature_cat/baseline.py
@@ -13,8 +13,8 @@ hparams = {
         "init": {},
     },
     "config": {
-        "seizure_weights": ...,
-        "pdrda_weights": ...,
+        "seizure_weights": "./artifacts/05_seizure_classification-baseline-v1-7a6a785852194b58b19971f61c7b5977/train/model_weights/epoch=3-step=5284.ckpt",
+        "pdrda_weights": "./artifacts/06_pdrda_classification-baseline_low-v4-e568535bf4274c94a00c2b78ba4c9dc2/train/model_weights/epoch=2-step=15852.ckpt",
         "data_dir": "./data/hms/train_eegs",
         "train_ann": "./data/processed/patient_split/train_patient_ids.csv",
         "val_ann": "./data/processed/patient_split/val_patient_ids.csv",
