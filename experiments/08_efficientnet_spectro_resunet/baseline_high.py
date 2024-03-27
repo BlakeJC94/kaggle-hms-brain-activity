95e93661fdf786289151ced2c4baecd502106bc6
BlakeJC94 <blakejamescook@gmail.com>
2024-03-27 19:45:34 +0100

    Fix imports
    Former-commit-id: 85665923788aeba7e147d939da0ec090f876a99d
    Former-commit-id: f576e120b30af8cfa420c47bf7040ef4a8f203f0

--- experiments/08_efficientnet_spectro_resunet/baseline_high.py
+++ experiments/08_efficientnet_spectro_resunet/baseline_high.py
@@ -1,4 +1,4 @@
 from core.utils import import_script_as_module
 
-hparams = import_script_as_module("./experiments/02_efficientnet_spectro/baseline.py").hparams
+hparams = import_script_as_module("./experiments/08_efficientnet_spectro_resunet/baseline.py").hparams
 hparams["config"]["learning_rate"] = 5 * 1e-3
