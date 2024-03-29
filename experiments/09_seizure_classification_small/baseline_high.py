a574e799d123624cddb851a0ad90e89fc774ad4e
BlakeJC94 <blakejamescook@gmail.com>
2024-03-29 08:38:01 +0100

    whoops
    Former-commit-id: 664db15f7e2418118aaebd0027720ac6184a3661
    Former-commit-id: f5d76eed6c16f5da8b210ee300a27f9cafc53834

--- experiments/09_seizure_classification_small/baseline_high.py
+++ experiments/09_seizure_classification_small/baseline_high.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from core.utils import import_script_as_module
 
-hparams = import_script_as_module(Path(__file__) / "baseline.py").hparams
-hparams = import_script_as_module("./experiments/08_efficientnet_spectro_resunet/baseline.py").hparams
+hparams = import_script_as_module(Path(__file__).parent / "baseline.py").hparams
 hparams["config"]["learning_rate"] = 3 * 1e-3
